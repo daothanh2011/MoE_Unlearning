15a8c8012b52956713d46bf505ce3678f87e85d4
skydvn <mduongbkhn@gmail.com>
2026-04-27 14:32:35 +0700

    CORAL

--- domainbed/algorithms.py
+++ domainbed/algorithms.py
@@ -437,24 +437,103 @@ class ERM_CIRL(ERM):
         return self.classifier(self.featurizer(x))
 
 
-# --- inside domainbed/algorithms.py ---
-#
-# Drop-in addition: ERM_MatchDG -- DomainBed's standard ERM (a generic
-# featurizer + linear classifier) trained with the MatchDG objective from
-# Mahajan, Tople & Sharma (ICML 2021), "Domain Generalization using Causal
-# Matching" (https://arxiv.org/pdf/2006.07500).
-#
-# Place this block anywhere after `class ERM` is defined in algorithms.py.
-# It also requires this import at the top of algorithms.py:
-#
-#     from domainbed.losses.matchdg_utils import (
-#         supervised_contrastive_loss,
-#         find_cross_domain_matches,
-#         matching_l2_loss,
-#         ProjectionHead,
-#     )
-#
-# and 'ERM_MatchDG' added to the ALGORITHMS list.
+class AbstractMMD(ERM):
+    """
+    Perform ERM while matching the pair-wise domain feature distributions
+    using MMD (abstract class)
+    """
+    def __init__(self, input_shape, num_classes, num_domains, hparams, gaussian):
+        super(AbstractMMD, self).__init__(input_shape, num_classes, num_domains,
+                                  hparams)
+        if gaussian:
+            self.kernel_type = "gaussian"
+        else:
+            self.kernel_type = "mean_cov"
+
+    def my_cdist(self, x1, x2):
+        x1_norm = x1.pow(2).sum(dim=-1, keepdim=True)
+        x2_norm = x2.pow(2).sum(dim=-1, keepdim=True)
+        res = torch.addmm(x2_norm.transpose(-2, -1),
+                          x1,
+                          x2.transpose(-2, -1), alpha=-2).add_(x1_norm)
+        return res.clamp_min_(1e-30)
+
+    def gaussian_kernel(self, x, y, gamma=[0.001, 0.01, 0.1, 1, 10, 100,
+                                           1000]):
+        D = self.my_cdist(x, y)
+        K = torch.zeros_like(D)
+
+        for g in gamma:
+            K.add_(torch.exp(D.mul(-g)))
+
+        return K
+
+    def mmd(self, x, y):
+        if self.kernel_type == "gaussian":
+            Kxx = self.gaussian_kernel(x, x).mean()
+            Kyy = self.gaussian_kernel(y, y).mean()
+            Kxy = self.gaussian_kernel(x, y).mean()
+            return Kxx + Kyy - 2 * Kxy
+        else:
+            mean_x = x.mean(0, keepdim=True)
+            mean_y = y.mean(0, keepdim=True)
+            cent_x = x - mean_x
+            cent_y = y - mean_y
+            cova_x = (cent_x.t() @ cent_x) / (len(x) - 1)
+            cova_y = (cent_y.t() @ cent_y) / (len(y) - 1)
+
+            mean_diff = (mean_x - mean_y).pow(2).mean()
+            cova_diff = (cova_x - cova_y).pow(2).mean()
+
+            return mean_diff + cova_diff
+
+    def update(self, minibatches, unlabeled=None):
+        objective = 0
+        penalty = 0
+        nmb = len(minibatches)
+
+        features = [self.featurizer(xi) for xi, _ in minibatches]
+        classifs = [self.classifier(fi) for fi in features]
+        targets = [yi for _, yi in minibatches]
+
+        for i in range(nmb):
+            objective += F.cross_entropy(classifs[i], targets[i])
+            for j in range(i + 1, nmb):
+                penalty += self.mmd(features[i], features[j])
+
+        objective /= nmb
+        if nmb > 1:
+            penalty /= (nmb * (nmb - 1) / 2)
+
+        self.optimizer.zero_grad()
+        (objective + (self.hparams['mmd_gamma']*penalty)).backward()
+        self.optimizer.step()
+
+        if torch.is_tensor(penalty):
+            penalty = penalty.item()
+
+        return {'loss': objective.item(), 'penalty': penalty}
+
+
+class MMD(AbstractMMD):
+    """
+    MMD using Gaussian kernel
+    """
+
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(MMD, self).__init__(input_shape, num_classes,
+                                          num_domains, hparams, gaussian=True)
+
+
+class CORAL(AbstractMMD):
+    """
+    MMD using mean and covariance difference
+    """
+
+    def __init__(self, input_shape, num_classes, num_domains, hparams):
+        super(CORAL, self).__init__(input_shape, num_classes,
+                                         num_domains, hparams, gaussian=False)
+
 
 class MatchDG(ERM):
     def __init__(self, input_shape, num_classes, num_domains, hparams):
